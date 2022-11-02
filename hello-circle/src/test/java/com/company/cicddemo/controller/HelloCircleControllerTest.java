522e5e766cb8c653ad590e3f6465402bd7090049
rich-f-p <richardferry3@gmail.com>
2022-11-02 16:50:30 -0400

    make test pass

--- hello-circle/src/test/java/com/company/cicddemo/controller/HelloCircleControllerTest.java
+++ hello-circle/src/test/java/com/company/cicddemo/controller/HelloCircleControllerTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HelloCircleControllerTest.class)
+@WebMvcTest(HelloCircleController.class)
 public class HelloCircleControllerTest {
 
     @Autowired
