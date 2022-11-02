562c8d086b279aacf8e9c97b06e4eabc4bf74318
rich-f-p <richardferry3@gmail.com>
2022-11-02 15:27:29 -0400

    initial commit

---
+++ ci-cd-demo/src/test/java/com/company/cicddemo/CiCdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.cicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
